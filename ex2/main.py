3ff8d07a5492d2a64581e3e439aee03c3ff60afd
elchanan <schnaidman.elchanan@mail.huji.ac.il>
2024-02-04 23:00:08 +0200

    most likely tag baseline function. b section DONE.

--- ex2/main.py
+++ ex2/main.py
@@ -8,10 +8,13 @@ from collections import Counter
 #nl.download('averaged_perceptron_tagger')
 #nl.download('punkt')
 START_TOKEN = 'STARTSTART'
-start_token_tagged = nl.pos_tag(nl.word_tokenize(START_TOKEN))
+START_TAG = 'STARTTAG'
+#start_token_tagged = nl.pos_tag(nl.word_tokenize(START_TOKEN))
+start_token_tagged = [(START_TOKEN, START_TAG)]
 WORD_POSITION = 0
 TAG_POSITION = 1
 UNKNOWN_WORD_TAG = 'NN'
+SECTION = 'c'
 def contains_only_nonalphabetic(input_string):
     return not any(char.isalpha() for char in input_string)
 
@@ -69,33 +72,95 @@ def word_in_training_set_checker(training_set, tagged_word):
             return True
     return False
 
+
+def train_transition_hmm(training_set):
+    hmm = {key[TAG_POSITION]: {} for key in training_set}
+
+    # counting the number of appearances of each bigram
+    for i in range(len(training_set) - 1):
+        current_word = training_set[i]
+        next_word = training_set[i + 1]
+
+        if next_word[WORD_POSITION] == START_TOKEN:  # end of sentence
+            continue
+
+        sub_dict = hmm[current_word[TAG_POSITION]]
+
+        if next_word[TAG_POSITION] in sub_dict:
+            sub_dict[next_word[TAG_POSITION]] += 1
+        else:
+            sub_dict[next_word[TAG_POSITION]] = 1
+
+    for curr_tag_key in hmm:
+        sub_dict = hmm[curr_tag_key]
+        sum_sub_dict = sum(sub_dict.values())
+        for nex_tag_key in sub_dict:
+            sub_dict[nex_tag_key] /= sum_sub_dict
+
+    return hmm
+
+
+def train_emission_hmm(training_set):
+    tags_possible = {key[TAG_POSITION]: 0 for key in training_set}
+    hmm = {key[WORD_POSITION]: tags_possible for key in training_set}
+    training_set_tags = [tagged_word[TAG_POSITION] for tagged_word in training_set]
+    training_set_tags_with_amount = Counter(training_set_tags)
+    ################## this for loop adds value for the all tags for some reason - NOT GOOD!
+    for tagged_word in training_set:
+        t = hmm[tagged_word[WORD_POSITION]]
+        t[tagged_word[TAG_POSITION]] += 1
+
+    for word_emission in hmm.values():
+        for tag in word_emission:
+            word_emission[tag] /= training_set_tags_with_amount[tag]
+
+
+
+
 def main():
     [training_set, test_set] = import_db()
 
-    # b section - DONE
-    test_set_unique_words = list(set(tuple(t) for t in test_set))
-    test_set_unique_words.remove(start_token_tagged[0])
-    num_of_words = len(test_set_unique_words)
-    i = 0
-    correct_tags_known_words = 0
-    amount_known_words = 0
-    correct_tags_unknown_words = 0
-    amount_unknown_words = 0
-    for word in test_set_unique_words:
-        i += 1
-        if word_in_training_set_checker(training_set, word):
-            if word[TAG_POSITION] == most_likely_baseline(training_set, word):
-                correct_tags_known_words += 1
-            amount_known_words += 1
-        else:
-            if word[TAG_POSITION] == most_likely_baseline(training_set, word):
-                correct_tags_unknown_words += 1
-            amount_unknown_words += 1
+    if SECTION == 'b':
+        # b section - DONE
+        test_set_unique_words = list(set(tuple(t) for t in test_set))
+        test_set_unique_words.remove(start_token_tagged[0])
+        num_of_words = len(test_set_unique_words)
+        i = 0
+        correct_tags_known_words = 0
+        amount_known_words = 0
+        correct_tags_unknown_words = 0
+        amount_unknown_words = 0
+        for word in test_set_unique_words:
+            i += 1
+            if word_in_training_set_checker(training_set, word):
+                if word[TAG_POSITION] == most_likely_baseline(training_set, word):
+                    correct_tags_known_words += 1
+                amount_known_words += 1
+            else:
+                if word[TAG_POSITION] == most_likely_baseline(training_set, word):
+                    correct_tags_unknown_words += 1
+                amount_unknown_words += 1
 
-    print("The error rate for known words is   ", 1-correct_tags_known_words/amount_known_words)
-    print("The error rate for unknown words is ", 1-correct_tags_unknown_words/amount_unknown_words)
-    print("The total error rate is ", 1-(correct_tags_known_words + correct_tags_unknown_words)/(amount_unknown_words + amount_known_words))
-    pass
+        print("The error rate for known words is   ", 1-correct_tags_known_words/amount_known_words)
+        print("The error rate for unknown words is ", 1-correct_tags_unknown_words/amount_unknown_words)
+        print("The total error rate is ", 1-(correct_tags_known_words + correct_tags_unknown_words)/(amount_unknown_words + amount_known_words))
+
+    # c section
+    elif SECTION == 'c':
+        #transition_hmm = train_transition_hmm(training_set)
+        emission_hmm = train_emission_hmm((training_set))
+        pass
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
